2082516f6de67431e6d47c5c7a52c86cd31e7005
Sergey Mazovets <smazovets@gmail.com>
2022-09-06 21:44:17 +0700

    second lesson

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,47 +2,49 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Buttons;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
 
-  SpriteBatch batch;
-  Texture img;
-  Texture ufo;
-  int clicker;
+  private SpriteBatch batch;
+  private MyAnimation animatedSprite;
+  private int xCoordinate;
+  int direction = 1;
 
   @Override
   public void create() {
     batch = new SpriteBatch();
-    img = new Texture("space.png");
-    ufo = new Texture("ufo.png");
+    animatedSprite = new MyAnimation("assets/animation-sprite.jpg", 6, 1, PlayMode.LOOP);
   }
 
   @Override
   public void render() {
-    ScreenUtils.clear(1, 0, 0, 1);
+    ScreenUtils.clear(1, 1, 1, 1);
+    animatedSprite.setTime(Gdx.graphics.getDeltaTime());
+    xCoordinate = xCoordinate + direction;
 
-    float x = Gdx.input.getX() - 50;
-    float y = Gdx.graphics.getHeight() - Gdx.input.getY() - 50;
+    if (xCoordinate > (Gdx.graphics.getHeight())) {
+      direction = -1;
+    } else if (xCoordinate < 0) {
+      direction = 1;
+    }
 
-    if (Gdx.input.isButtonJustPressed(Buttons.LEFT)) {
-      clicker++;
-      Gdx.graphics.setTitle("Clicked " + clicker);
+    if (direction == -1 && !animatedSprite.getFrame().isFlipX()) {
+      animatedSprite.getFrame().flip(true, false);
+    } else if (direction == 1 && animatedSprite.getFrame().isFlipX()) {
+      animatedSprite.getFrame().flip(true, false);
     }
 
     batch.begin();
-    batch.draw(img, 0, 0);
-    batch.draw(ufo, x, y, 100, 100);
+    batch.draw(animatedSprite.getFrame(), xCoordinate, 0);
     batch.end();
   }
 
   @Override
   public void dispose() {
-    batch.dispose();
-    img.dispose();
+    animatedSprite.dispose();
   }
 
 }
